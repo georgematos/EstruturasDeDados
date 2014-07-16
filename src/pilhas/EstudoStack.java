3723b1b9b1a4abebb8a644fa4a8ec0ea96e9660c
George <george@nayalison-OptiPlex-7010.(none)>
2014-07-16 10:50:19 -0300

    Fim do estudo de Pilha

---
+++ src/pilhas/EstudoStack.java
@@ -0,0 +1,27 @@
+package pilhas;
+
+import java.util.Stack;
+
+public class EstudoStack {
+
+	public static void main(String[] args) {
+		
+		// Cria uma pilha de pecas
+		Stack<Peca> pilha = new Stack<>();
+		
+		// Cria uma peca e coloca na pilha
+		Peca pecaInserida = new Peca("corrente");
+		pilha.push(pecaInserida);
+
+		
+		// Remove a peca da pilha e imprime o nome da peca removida
+		Peca pecaRemovida = pilha.pop();					
+		System.out.println(pecaRemovida.getNome());
+		
+		// Verifica se a pilha esta vazia
+		if(pilha.isEmpty()) {
+			System.out.println("A pilha está vazia");
+		}
+		
+	}
+}
